96f981f6976598a5acad3745307db0b4b93fdd8f
Thapa-Pro <bachchan.k.t@gmail.com>
2026-03-22 10:08:08 +0100

    Basic setup with header and footer and homepage UI

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import "../styles/header.css";
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="headerInner">
+        <div className="brand">
+          <img src="/images/heart.png" alt="HabitTrack logo" className="logoIcon" />
+          <h1 className="brandText">HabitTrack</h1>
+        </div>
+
+        <nav className="nav">
+          <a href="#">Home</a>
+          <a href="#">Progress</a>
+          <a href="#">Stats</a>
+        </nav>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
